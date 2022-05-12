7b30096506fa28b36322c63de311d2bbb1008e65
suzhant <xresthasushant61@gmail.com>
2022-05-13 00:08:20 +0545

    completed registration form validations

--- app/src/main/java/com/sushant/fashionapp/ActivityRegister.java
+++ app/src/main/java/com/sushant/fashionapp/ActivityRegister.java
@@ -1,7 +1,6 @@
 package com.sushant.fashionapp;
 
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
